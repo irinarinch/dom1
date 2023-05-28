b4bf11e91b179596a20ab45657faf50c79aef6fe
irinarinch <you@example.com>
2023-05-29 06:47:16 +0800

    many changes

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,18 @@
 const { merge } = require ('webpack-merge');
 const common = require ('./webpack.common');
-const TerserPlugin = require ('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require ('optimize-css-assets-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge (common, {
   mode: 'production',
+  
   optimization: {
+    minimize: true,
     minimizer: [
+      new MiniCssExtractPlugin(),
       new TerserPlugin({}),
-      new OptimizeCSSAssetsPlugin({}),
+      
     ]
   },
+
 });
